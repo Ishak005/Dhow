64ad135a58503130a19d8594ebe270392a75fd2d
Ishak005 <ishaktwahir@gmail.com>
2024-08-03 20:33:02 +0300

    first commit

---
+++ Dhows/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+      <div className="text-xl font-medium text-black">Hello, Tailwind CSS!</div>
+      <p className="text-gray-500">This is a simple Tailwind CSS example.</p>
+    </div>
+  );
+}
+
+export default App;
